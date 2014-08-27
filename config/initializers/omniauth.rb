101f19a936ee4745fb46aff6a6cb75de0ebca0f2
Alex Kaldjian <alex.kaldjian@gmail.com>
2014-08-27 14:10:59 -0400

    Moar Oauth tinkering

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV['FB_APP_ID'], ENV['FB_SECRETKEY'], :image_size => 'large'
-	 {:scope => :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+	 # {:scope => :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end

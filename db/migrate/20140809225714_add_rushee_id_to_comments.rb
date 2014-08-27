b90f0211d14e3b8996b002f3e5f035205b47ee8e
Alex Kaldjian <alex.kaldjian@gmail.com>
2014-08-27 15:17:23 -0400

    Ugh

--- db/migrate/20140809225714_add_rushee_id_to_comments.rb
+++ db/migrate/20140809225714_add_rushee_id_to_comments.rb
@@ -2,6 +2,5 @@ class AddRusheeIdToComments < ActiveRecord::Migration
   def change
     add_column :comments, :rushee_id, :int
     add_column :comments, :body, :text
-    add_column :comments, :user_id, :int
   end
 end

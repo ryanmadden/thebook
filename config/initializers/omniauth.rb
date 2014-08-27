0f16c9023edb2db8775576b16410140df9c6344a
Alex Kaldjian <alex.kaldjian@gmail.com>
2014-08-27 14:18:41 -0400

    OAuth tinkering...

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FB_APP_ID'], ENV['FB_SECRETKEY'], :image_size => 'large'
-	 # {:scope => :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+	provider :facebook, ENV['FB_APP_ID'], ENV['FB_SECRETKEY'], :image_size => 'large', {client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
 end

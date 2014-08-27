dcbcfd9f3ff5039bb5f62f48ce2171bb8a236505
Alex Kaldjian <alex.kaldjian@gmail.com>
2014-08-27 14:08:48 -0400

    OAuth gitignore fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '1434599726817174', 'f71ba0e1777f9da8abd2c64659804fa6', :image_size => 'large'
+	provider :facebook, ENV['FB_APP_ID'], ENV['FB_SECRETKEY'], :image_size => 'large'
 	 {:scope => :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
